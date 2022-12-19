302b42400c268e817f0392dbee0da8dba4d55a09
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2022-12-19 16:24:37 -0500

    Created head.tsx for Head info

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+const Head = () => {
+  return (
+    <>
+        <title>Masaya Funakoshi Photography</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="../public/favicon.svg" />
+    </>
+  )
+}
+
+export default Head
