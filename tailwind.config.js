d7966ff7f024ca4030c9ccb284aff5c1d2672b90
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2022-12-21 18:09:52 -0500

    Added new styling and tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
+        letterSpacing: {
+            tightest: '-.075em',
+            tighter: '-.05em',
+            tight: '-.025em',
+            normal: '0',
+            wide: '.025em',
+            wider: '.05em',
+            widest: '.18em',
+        },
         extend: {
             colors:{
                 'primary-black': '#000000',
